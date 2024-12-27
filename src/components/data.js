7326ddd8444592928f9c747bf042d3be300d2f38
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-12-26 21:32:47 -0600

    all added

---
+++ src/components/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id:'1',
+    question:'what are accordion components?',
+    answer:
+    'Accordion components are user interface elements used for organizind and presention'
+  },
+  {
+    id:'2',
+    question:'what are the used for?',
+    answer:
+    'They are commonly employed in various context...'
+  },
+  {
+    id:'3',
+    question:'Accordion as a musical instrument',
+    answer:
+    "The accordion is a musical instrument with a keybord and bellows. It produces sounds.."
+  }
+]
+
+export default data
